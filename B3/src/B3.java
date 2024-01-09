e54440583cbd5393a3d01d47d6616a21197737df
Moriarty <tphong610198@gmail.com>
2024-01-09 14:30:07 +0700

    commited by Moriarty

--- B3/src/B3.java
+++ B3/src/B3.java
@@ -1,7 +1,5 @@
-package b3;
 public class B3 {
-
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!!!");
     }
 }
--- B3/src/outFormat.java
+++ B3/src/outFormat.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package b3;
-
-/**
- *
- * @author tphon
- */
-public class outFormat {
-    
+public class outFormat{
+    public static void main(String[] args){
+        String S="Hello";
+        int i=5;
+        long l=58;
+        float f= 7.2f;
+        double d= 8.9;
+        boolean b= true;
+        char c = 'A';
+        System.out.format("%s,%2d,%4Xh,%5.2f,%10.3f,%3c,%b\n",S,i,l,f,d,c,b);
+        System.out.format("%3$3b,%1$3d,%2$12s\n", i,S,b);
+    }
 }
