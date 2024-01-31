ce942672d696b05610f526c0c15c2d79028d40a6
Moriarty <tphong610198@gmail.com>
2024-01-31 21:21:37 +0700

    commited by Moriarty

--- LabPRO/src/Lab5_1.java
+++ LabPRO/src/Lab5_1.java
@@ -101,15 +101,114 @@
 //}
 
 
+//import java.util.*;
+//
+//class PhoneNumber {
+//    Scanner in = new Scanner(System.in);
+//    
+//    int area;
+//    String number;
+//
+//    public PhoneNumber() {
+//    }
+//
+//    public PhoneNumber(int area, String number) {
+//        this.area = area;
+//        this.number = number;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.area + " - " + this.number;
+//    }
+//
+//    public PhoneNumber inputData() {
+//        // System.out.print("Enter area code: ");
+//        int areaCode = in.nextInt();
+//        // System.out.print("Enter number: ");
+//        String phoneNumber = in.next();
+//        return new PhoneNumber(areaCode, phoneNumber);
+//    }
+//
+//    void display() {
+//        System.out.println("Area: " + area);
+//        System.out.println("Phone number: " + number);
+//    }
+//}
+//
+//
+//
+//
+//class IntPhoneNumber extends PhoneNumber {
+//    String countryCode;
+//
+//    public IntPhoneNumber() {
+//        super();
+//    }
+//
+//    public IntPhoneNumber(String countryCode, int area, String number) {
+//        super(area, number);
+//        this.countryCode = countryCode;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return this.countryCode + " - " + super.toString();
+//    }
+//
+//    public IntPhoneNumber inputData() {
+//        // System.out.print("Enter country code: ");
+//        String countryCode = in.next();
+//        PhoneNumber localPhone = super.inputData();
+//        return new IntPhoneNumber(countryCode, localPhone.area, localPhone.number);
+//    }
+//
+//    @Override
+//    public void display() {
+//        super.display();
+//        System.out.println("Country code: " + countryCode);
+//    }
+//}
+//
+//public class Lab5_1 {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        // PhoneNumber[] phonelist = new PhoneNumber[1];
+//        // int i = 0;
+//        int check = scan.nextInt();
+//        if (check == 1) {
+//            // System.out.println("Type of phone number - 1. Local phone; - 2. Inter phone - 0. Exit");
+//            int typeOfPhone = scan.nextInt();
+//
+//            if (typeOfPhone == 1) {
+//                PhoneNumber phone = new PhoneNumber();
+//                PhoneNumber inputData = phone.inputData();
+//                // i++;
+//                System.out.println("Add local phone number successfully!");
+//                inputData.display();
+//            }
+//            else if (typeOfPhone == 2) {
+//                IntPhoneNumber intPhone = new IntPhoneNumber();
+//                IntPhoneNumber inputData = intPhone.inputData();
+//                // i++;
+//                System.out.println("Add inter phone number successfully! ");
+//                inputData.display();
+//            }
+//        }
+//    }
+//}
+
+
 import java.util.*;
 
 class PhoneNumber {
-    Scanner in = new Scanner(System.in);
-    
+    //Scanner in = new Scanner(System.in);
+    Scanner in;
     int area;
     String number;
 
-    public PhoneNumber() {
+    public PhoneNumber(Scanner in) {
+        this.in=in;
     }
 
     public PhoneNumber(int area, String number) {
@@ -131,7 +230,7 @@ class PhoneNumber {
     }
 
     void display() {
-        System.out.println("Area: " + area);
+        System.out.println("Area code: " + area);
         System.out.println("Phone number: " + number);
     }
 }
@@ -142,8 +241,8 @@ class PhoneNumber {
 class IntPhoneNumber extends PhoneNumber {
     String countryCode;
 
-    public IntPhoneNumber() {
-        super();
+    public IntPhoneNumber(Scanner in) {
+        super(in);
     }
 
     public IntPhoneNumber(String countryCode, int area, String number) {
@@ -174,23 +273,22 @@ class IntPhoneNumber extends PhoneNumber {
 
 public class Lab5_1 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         // PhoneNumber[] phonelist = new PhoneNumber[1];
         // int i = 0;
-        int check = scan.nextInt();
+        Scanner in = new Scanner(System.in);
+        int check = in.nextInt();
         if (check == 1) {
             // System.out.println("Type of phone number - 1. Local phone; - 2. Inter phone - 0. Exit");
-            int typeOfPhone = scan.nextInt();
-
+            int typeOfPhone = in.nextInt();
             if (typeOfPhone == 1) {
-                PhoneNumber phone = new PhoneNumber();
+                PhoneNumber phone = new PhoneNumber(in);
                 PhoneNumber inputData = phone.inputData();
                 // i++;
                 System.out.println("Add local phone number successfully!");
                 inputData.display();
             }
             else if (typeOfPhone == 2) {
-                IntPhoneNumber intPhone = new IntPhoneNumber();
+                IntPhoneNumber intPhone = new IntPhoneNumber(in);
                 IntPhoneNumber inputData = intPhone.inputData();
                 // i++;
                 System.out.println("Add inter phone number successfully! ");
