daa3c8ce68bc7ca743747f75ae0a7ce986b61344
Moriarty <tphong610198@gmail.com>
2024-02-14 15:33:42 +0700

    commited by Moriarty

--- TestCode/src/Test14_2_2024.java
+++ TestCode/src/Test14_2_2024.java
@@ -1,35 +1,7 @@
 public class Test14_2_2024 {
-    public static int foo(int a, String s)
-
-{
-
-s = "Yellow";
-
-a=a+2;
-
-return a;
-
-}
-
-public static void bar()
-
-{
-
-int a=3;
-
-String s = "Blue";
-
-a = foo(a,s);
-
-System.out.println("a="+a+" s="+s);
-
-}
-
-public static void main(String args[])
-
-{
-
-bar();
-
-}
+    public static void main(String args[]){
+        int i = 0;
+        i = i++ + i;
+    System.out.println("I = " +i);
+    }
 }
