05e3e2b0d1421a3e2f05a4f1dbf6972fadeab89e
Moriarty <tphong610198@gmail.com>
2024-03-11 11:35:50 +0700

    commited by Moriarty

--- CollectionFrameWork/src/Organization.java
+++ CollectionFrameWork/src/Organization.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public abstract class Organization {
     private int size;
     public Organization(){}
     
-    public Organization(String str){}
+    public Organization(int size){
+        this.size = size;
+    }
     
     public int getSize(){
         return size;
@@ -11,5 +14,15 @@ public abstract class Organization {
     
     public void setSize(int size){
         this.size = size;
+    }
+
+    public void input(Scanner scan){
+        size = scan.nextInt();
+    }
+
+    public abstract void communicateByTool();
+
+    public String toString(){
+        return "The organization's size is " + size;
     }
 }
